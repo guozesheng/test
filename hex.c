11d69a47f395039c127522b803c9242baaceadf6
XiaoGuo <guozesheng@gmail.com>
2011-04-07 19:25:32 +0800

    hex

--- hex.c
+++ hex.c
@@ -1,35 +1,22 @@
 #include <stdio.h>
 
-void cov(int num);
-
 int main(int argc, const char *argv[])
 {
     int num;
+    int i = 28;
+    unsigned int mask = 0x0f;
+    char str_h[16] = "0123456789ABCDEF";
+
     printf("Please input a numer:");
     scanf("%d", &num);
 
-    cov(num);
+    while (i -= 4) 
+    {
+        printf("%c", str_h[num>>i&mask]);
+    }
+    printf("%c", str_h[num>>i&mask]);
+
     printf("\n");
 
     return 0;
-}
-
-void cov(int num)
-{
-    int tmp;
-
-    if (num / 16) 
-    {
-        cov(num / 16);
-    }
-    tmp = num % 16;
-    if (tmp <= 9) 
-    {
-        printf("%d", tmp);
-    }
-    else 
-    {
-        printf("%c", 'A' + tmp - 10);
-    }
-    
 }
