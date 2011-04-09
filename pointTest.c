c918d99dc6292fd1f0d94ddc45fcdd5132bde184
XiaoGuo <guozesheng@gmail.com>
2011-04-09 16:43:01 +0800

    pointTest modify

--- pointTest.c
+++ pointTest.c
@@ -4,6 +4,8 @@ int main(int argc, const char *argv[])
 {
     char str[20] = "This is a C world!";
     char *p = str;
+    char cStr[20];
+    char *pc = cStr;
     
 //*
     while (*p != '\0') 
@@ -19,13 +21,25 @@ int main(int argc, const char *argv[])
         printf("%c", *p);
     }
 //*/
-    
+
 /*
     for (p += sizeof(str); p >= str; p--) 
     {
         putchar(*p);
     }
 //*/
+
+    printf("\n");
+    for (p = str; *p != '\0'; p++) 
+    {
+        if (*p != ' ') 
+        {
+            *pc++ = *p;
+        }
+    }
+    *pc = '\0';
+
+    puts(cStr);
 
     printf("\n");
     return 0;
