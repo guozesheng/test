758d20218b467d508552c449818d20f4a1eb9a4f
XiaoGuo <guozesheng@gmail.com>
2011-04-17 14:16:53 +0800

    linkList.c

--- linkList.c
+++ linkList.c
@@ -1,27 +1,59 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 typedef struct node
 {
     int num;
-    char name[20];
     char level;
+    struct node *next;
 }NODE;
+
+NODE *createLink(int n);
+void printList(NODE *p);
 
 int main(int argc, const char *argv[])
 {
-    NODE stu;
-    NODE *ptr;
-    ptr = &stu;
-
-    printf("Input the number:");
-    scanf("%d", &ptr->num);
-    printf("Input the name:");
-    scanf("%s", ptr->name);
-    getchar();
-    printf("Input the level:");
-    scanf("%c", &ptr->level);
-    printf("number: %d, name: %s, level: %c\n", ptr->num, ptr->name, ptr->level);
+    NODE *head;
+    head = createLink(10);
+    printList(head);
     
     return 0;
+}
+
+NODE *createLink(int n)
+{
+    NODE *p, *head;
+    int i;
+
+    if ((head = malloc(sizeof(NODE))) == NULL)
+    {
+        return NULL;
+    }
+    head -> num = 1;
+    head -> level = 'A';
+    head -> next = NULL;
+    p = head;
+
+    for (i = 1; i < n; i++) 
+    {
+        if ((p->next = malloc(sizeof(NODE))) == NULL)
+        {
+            return NULL;
+        }
+        p -> next -> num = i + 1;
+        p -> next -> level = 'A';
+        p -> next -> next = NULL;
+        p = p -> next;
+    }
+
+    return head;
+}
+
+void printList(NODE *p)
+{
+    do 
+    {
+        printf("num: %d\tlevel: %c\n", p->num, p->level);
+        p = p->next;
+    } while (p);
 }
