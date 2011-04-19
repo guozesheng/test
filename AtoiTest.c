728487a0f8842b5474320ddfbe103a9321dcb8a2
XiaoGuo <guozesheng@gmail.com>
2011-04-19 18:42:49 +0800

    AtoiTest.c

--- AtoiTest.c
+++ AtoiTest.c
@@ -3,16 +3,7 @@
 
 int main(int argc, const char *argv[])
 {
-    char a[10] = "";
-    int i;
-    long int j;
-    printf("Input the number:\n");
-    scanf("%s", a);
-    getchar();
-    printf("Input the number system:\n");
-    scanf("%d", &i);
-    j = strtol(a, NULL, i);
-    printf("%ld\n", j);
+    printf("%ld\n", strtol(argv[1], NULL, atoi(argv[2])));
 
     return 0;
 }
