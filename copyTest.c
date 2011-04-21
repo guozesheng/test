9f8b41110fafe0bb106a9f1e1ab808d4d376d57c
XiaoGuo <guozesheng@gmail.com>
2011-04-21 19:44:52 +0800

    copyTest.c

--- copyTest.c
+++ copyTest.c
@@ -5,13 +5,19 @@ int main(int argc, const char *argv[])
 {
     FILE *fp, *fp2;
     char sHello[1024];
-    int n;
+    int n = 0;
 
     if (((fp = fopen("a.text", "r")) == NULL) || ((fp2 = fopen("a2.text", "w+")) == NULL)) 
     {
         perror("fopen:");
+        exit(1);
     }
-    n = fread(sHello, 1, 1024, fp);
+    while (!feof(fp)) 
+    {
+        fread(&sHello[n], 1, 1, fp);
+        n++;
+    }
+    n--;
     if (n != 0) 
     {
         sHello[n] = '\0';
@@ -20,5 +26,6 @@ int main(int argc, const char *argv[])
 
     fclose(fp);
     fclose(fp2);
+    printf("%d\n", n);
     return 0;
 }
