da28e242daa306e63d1b7a23cf71f79ca44101fd
XiaoGuo@laptop <guozesheng@gmail.com>
2011-04-21 17:14:42 +0800

    studyC/curlyBraces.c

---
+++ studyC/curlyBraces.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    char a[10] = {"abcde"};
+    char b[10] = "abcde";
+
+    printf("%s\n", a);
+    printf("%s\n", b);
+
+    return 0;
+}
