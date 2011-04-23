7c92ff004dfd5a3b7555a413dd0eb773c11b0b59
xiaoguo@qiao <guozesheng@gmail.com>
2011-04-23 16:49:09 +0800

    modified: five/*

--- five/main.h
+++ five/main.h
@@ -16,5 +16,7 @@ typedef struct
 int init_screen(void);
 int fb_one_pixel(int x, int y, u32_t color);
 int fb_line(int x1, int y1, int x2, int y2, u32_t color);
+int fb_circle(int x0, int y0, int r, u32_t color);
+int fb_circle_s(int x0, int y0, int r, u32_t color);
     
 #endif
