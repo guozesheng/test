f37c3624ddbdf95db06f9910fd8c0fbabc1f500a
xiaoguo@qiao <guozesheng@gmail.com>
2011-04-23 19:18:51 +0800

    five/mouse.c

---
+++ five/mouse_op.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "main.h"
+
+extern fbsrc_t fb_v;
+
+static u32_t cursor_pixel[C_W * C_H] = 
+{
+    BORD,T___,T___,T___,T___,T___,T___,T___,T___,T___,
+    BORD,BORD,T___,T___,T___,T___,T___,T___,T___,T___,  
+    BORD,X___,BORD,T___,T___,T___,T___,T___,T___,T___,
+    BORD,X___,X___,BORD,T___,T___,T___,T___,T___,T___,
+    BORD,X___,X___,X___,BORD,T___,T___,T___,T___,T___,
+    BORD,X___,X___,X___,X___,BORD,T___,T___,T___,T___,
+    BORD,X___,X___,X___,X___,X___,BORD,T___,T___,T___,
+    BORD,X___,X___,X___,X___,X___,X___,BORD,T___,T___,
+    BORD,X___,X___,X___,X___,X___,X___,X___,BORD,T___,
+    BORD,X___,X___,X___,X___,X___,X___,X___,X___,BORD,
+    BORD,X___,X___,X___,X___,X___,BORD,BORD,BORD,BORD,
+    BORD,X___,X___,BORD,X___,X___,BORD,T___,T___,T___,
+    BORD,X___,BORD,T___,BORD,X___,X___,BORD,T___,T___,
+    BORD,BORD,T___,T___,BORD,X___,X___,BORD,T___,T___,
+    T___,T___,T___,T___,T___,BORD,X___,X___,BORD,T___,
+    T___,T___,T___,T___,T___,BORD,X___,X___,BORD,T___,
+    T___,T___,T___,T___,T___,T___,BORD,BORD,T___,T___
+};
+
+int draw_cursor(int x, int y)
+{
+    int i = 0;
+    int j = 0;
+
+    for (j = 0; j < C_H; j++) 
+    {
+        for (i = 0; i < C_W; i++) 
+        {
+            if (cursor_pixel[i+j*C_W] != T___) 
+            {
+                fb_one_pixel(x+i, y+j, cursor_pixel[i+j*C_W]);
+            }
+        }
+    }
+
+    return 0;
+}
