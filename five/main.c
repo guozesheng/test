795bc0ed288af0db85910a62fb741b16dda7da8c
xiaoguo@qiao <guozesheng@gmail.com>
2011-04-23 15:46:51 +0800

    five

--- five/main.c
+++ five/main.c
@@ -4,7 +4,11 @@
 int main(int argc, const char *argv[])
 {
     init_screen();
-    fb_one_pixel(300, 300, 0x0000ff00);
+    //fb_one_pixel(300, 300, 0x0000ff00);
+    //fb_line(300, 300, 100, 100, 0x0000ff00);
+    //fb_line(0, 600, 1000, 400, 0x0000ff00);
+    //fb_line(300, 300, 400, 1000, 0x0000ff00);
+    fb_line(300, 300, 200, 1000, 0x0000ff00);
 
     return 0;
 }
--- five/main.h
+++ five/main.h
@@ -15,5 +15,6 @@ typedef struct
 
 int init_screen(void);
 int fb_one_pixel(int x, int y, u32_t color);
+int fb_line(int x1, int y1, int x2, int y2, u32_t color);
     
 #endif
