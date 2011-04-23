49c0add4eff41d8cd4a0e8507f6703d508c668fb
xiaoguo@qiao <guozesheng@gmail.com>
2011-04-23 11:29:03 +0800

    add five/*

---
+++ five/main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+typedef unsigned int u32_t;
+typedef unsigned char u8_t;
+
+typedef struct
+{
+    int w;
+    int h;
+    int bpp;
+
+    void *memo;
+} fbsrc_t;
+
+int init_screen(void);
+    
+#endif
