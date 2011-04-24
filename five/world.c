aa048419166440da71112d5cf840395c8456604a
xiaoguo@qiao <guozesheng@gmail.com>
2011-04-24 20:57:45 +0800

    five

--- five/world.c
+++ five/world.c
@@ -20,7 +20,66 @@ static u32_t num_1[16*16] =
     T___,T___,T___,T___,READ,READ,READ,READ,READ,READ,READ,READ,T___,T___,T___,T___,
 };
 
-//int write_world(int x, int y, u32_t *wr)
+static u32_t num_2[16*16]=
+{
+    T___,T___,T___,T___,T___,READ,READ,READ,READ,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,READ,READ,READ,READ,READ,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,READ,T___,T___,T___,READ,READ,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,T___,T___,READ,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,T___,T___,READ,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,T___,READ,READ,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,T___,READ,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,READ,READ,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,READ,T___,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,READ,READ,T___,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,READ,T___,T___,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,READ,READ,T___,T___,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,READ,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,READ,READ,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,READ,READ,READ,READ,READ,READ,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,READ,READ,READ,READ,READ,READ,T___,T___,T___,T___,T___,T___,
+};
+
+static u32_t num_i[16*16] = 
+{
+    T___,T___,T___,T___,T___,READ,READ,READ,READ,READ,READ,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,READ,READ,READ,READ,READ,READ,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,READ,READ,READ,READ,READ,READ,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,READ,READ,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,READ,READ,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,READ,READ,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,READ,READ,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,READ,READ,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,READ,READ,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,READ,READ,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,READ,READ,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,READ,READ,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,READ,READ,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,READ,READ,READ,READ,READ,READ,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,READ,READ,READ,READ,READ,READ,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,READ,READ,READ,READ,READ,READ,T___,T___,T___,T___,T___,
+};
+
+static u32_t num_w[16*16] = 
+{
+    READ,READ,READ,T___,T___,T___,READ,READ,READ,T___,T___,T___,READ,READ,READ,T___,
+    READ,READ,READ,T___,T___,T___,READ,READ,READ,T___,T___,T___,READ,READ,READ,T___,
+    T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,T___,
+    T___,T___,T___,T___,READ,T___,T___,T___,T___,T___,READ,T___,T___,T___,T___,T___,
+};
+
 int write_world(int x, int y, int num)
 {
     int i = 0;
@@ -35,7 +94,7 @@ int write_world(int x, int y, int num)
     }
     else if (num == 2) 
     {
-        wr = num_1;
+        wr = num_2;
     }
 
     for (j = 0; j < wr_h; j++) 
