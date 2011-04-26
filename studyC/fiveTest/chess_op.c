b8d5d1720c6de7fc1f195f4255d50846e00015c6
xiaoguo@laptop <guozesheng@gmail.com>
2011-04-26 23:02:01 +0800

    modified: fiveTest

--- studyC/fiveTest/chess_op.c
+++ studyC/fiveTest/chess_op.c
@@ -2,6 +2,8 @@
 
 extern int mx;  // extern from mouse_op.c
 extern int my;
+char curent_player = 1;
+u32_t curent_color = BLACK;
 
 int chess_do(void)
 {
@@ -16,10 +18,21 @@ int chess_do(void)
         return 0;
     }
 
-    x = ST_X + ((mx - ST_X) / SPACE) * SPACE + SPACE / 2;
+    x = ST_X + ((mx - ST_X) / SPACE) * SPACE + SPACE / 2;   // draw circle at center
     y = ST_Y + ((my - ST_Y) / SPACE) * SPACE + SPACE / 2;
 
-    fb_circle_fill(x, y, C_R, WHITE);
+    if (curent_player == 1) 
+    {
+        fb_circle_fill(x, y, C_R, curent_color);
+        curent_player = 2;
+        curent_color = WHITE;
+    }
+    else 
+    {
+        fb_circle_fill(x, y, C_R, curent_color);
+        curent_player = 1;
+        curent_color = BLACK;
+    }
 
     return 0;
 }
