eb6afe900faa9997e080144e7fc8ec92ffdc7e6d
xiaoguo@laptop <guozesheng@gmail.com>
2011-04-26 21:32:36 +0800

    modified: fiveTest

--- studyC/fiveTest/board_print.c
+++ studyC/fiveTest/board_print.c
@@ -2,7 +2,15 @@
 
 int board_print(u32_t color_line, u32_t color_bg)
 {
-    int i;
+    int i, j;
+
+    for (j = ST_Y - SPACE; j < ST_Y+B_X*SPACE; j++) 
+    {
+        for (i = ST_X - SPACE*2; i < ST_X + (B_Y+2)*SPACE; i++) 
+        {
+            fb_pixel(i, j, color_bg);
+        }
+    }
 
     for (i = 0; i < B_Y; i++) 
     {
