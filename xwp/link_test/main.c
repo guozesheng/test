5479472122ceda2702544e266e3ba597579557f2
xiaoguo@qiao <guozesheng@gmail.com>
2011-05-03 20:06:57 +0800

    xwp/link_test

---
+++ xwp/link_test/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "stack.h"
+
+int main(int argc, const char *argv[])
+{
+    push('a');
+    
+    return 0;
+}
---
+++ xwp/link_test/stack/stack.h
@@ -0,0 +1,8 @@
+#ifndef STACK_H
+#define STACK_H
+
+extern void push(char);
+extern char pop(void);
+extern int is_empty(void);
+
+#endif
