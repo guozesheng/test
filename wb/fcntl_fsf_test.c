524b9a58499bb3c8650cab54bfcdeb4542a913ab
XiaoGuo@xwp <guozesheng@gmail.com>
2011-05-07 23:55:06 +0800

    wb/fcntl_*.c

---
+++ wb/fcntl_fsf_test.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, const char *argv[])
+{
+    int val;
+
+    if (argc != 2) 
+    {
+        fputs("usae: a.out < descriptor#>\n", stderr);
+        exit(1);
+    }
+    if ((val = fcntl(atoi(argv[1]), F_GETFL)) < 0) 
+    {
+        printf("fcntl error for fd %d\n", atoi(argv[1]));
+        exit(1);
+    }
+
+    switch (val & O_ACCMODE)
+    {
+        case O_RDONLY:
+            printf("read only");
+            break;
+        case O_WRONLY:
+            printf("write only");
+            break;
+        case O_RDWR:
+            printf("read write");
+            break;
+        default:
+            fputs("invalid access mode\n", stderr);
+            exit(1);
+    }
+
+    if (val & O_APPEND) 
+    {
+        printf(", append");
+    }
+    if (val & O_NONBLOCK) 
+    {
+        printf(", nonblocking");
+    }
+
+    printf("\n");
+    return 0;
+}
