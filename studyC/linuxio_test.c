60b89f6b45d241a3fd966db690ea5c833f263c8b
XiaoGuo@xwp <guozesheng@gmail.com>
2011-05-07 20:37:08 +0800

    modified: studyC/linuxio_test.c

--- studyC/linuxio_test.c
+++ studyC/linuxio_test.c
@@ -5,15 +5,16 @@
 int main(int argc, const char *argv[])
 {
     int fd, fd_w;
-    //char str[] = "Hello, world!";
     char temp[4096];
     int size;
 
-    fd = open("a.text", O_RDONLY);
-    fd_w = open("a.write", O_WRONLY | O_CREAT, 0666);
+    fd = open(argv[1], O_RDONLY);
+    fd_w = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
 
-    size = read(fd,temp, 100);
-    write(fd_w, temp, size);
+    while ((size = read(fd,temp, 1024)))
+    {
+        write(fd_w, temp, size);
+    }
 
     close(fd);
     close(fd_w);
