09ed5ae9b6561d05f57b5b9c5bbe7de3f1a2e75c
XiaoGuo@xwp <guozesheng@gmail.com>
2011-05-07 22:44:37 +0800

    wb/

---
+++ wb/lseek_test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, const char *argv[])
+{
+    int fd;
+
+    if ((fd = open("a.text", O_RDONLY | O_WRONLY)) < 0)
+    {
+        perror("open fail");
+        exit(1);
+    }
+
+    lseek(fd, 2, SEEK_SET);
+
+    write(fd, "a", 1);
+
+    close(fd);
+    return 0;
+}
