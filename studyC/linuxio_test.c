2fbf7361100be81eceaa22851007ae7f24038435
XiaoGuo@xwp <guozesheng@gmail.com>
2011-05-07 20:48:48 +0800

    modified: studyC/linuxio_test.c

--- studyC/linuxio_test.c
+++ studyC/linuxio_test.c
@@ -17,6 +17,12 @@ int main(int argc, const char *argv[])
     fd = open(argv[1], O_RDONLY);
     fd_w = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
 
+    if (fd < 0 || fd_w < 0) 
+    {
+        printf("open file error\n");
+        exit(1);
+    }
+
     while ((size = read(fd,temp, 1024)))
     {
         write(fd_w, temp, size);
