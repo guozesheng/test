5f0dade466837cdc9c596306b9606465015cbeb5
xiaoguo@laptop <guozesheng@gmail.com>
2011-05-07 08:18:17 +0800

    studyC/struct_memory.c

---
+++ studyC/struct_memory.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    struct struct_memory_tt 
+    {
+        short int b;
+        char a[1];
+        //char b;
+    } str;
+    struct struct_memory_t 
+    {
+        int i;
+        char a;
+    } my_str;
+
+    printf("%d\n", sizeof(str));
+    printf("%d\n", sizeof(my_str));
+
+    return 0;
+}
