f893286231b0149a4f56c162474a7ed75ddffe84
XiaoGuo@xwp <guozesheng@gmail.com>
2011-05-27 20:56:59 +0800

    wb/pthread

---
+++ wb/pthread_conditionVariable_test.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+struct msg 
+{
+    int num;
+    struct msg *next;
+} ;
+struct msg *head;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t has_product = PTHREAD_COND_INITIALIZER;
+
+void *producer(void *p);
+void *consumer(void *p);
+
+int main(int argc, const char *argv[])
+{
+    pthread_t pid, cid;
+
+    srand(time(NULL));
+
+    pthread_create(&pid, NULL, producer, NULL);
+    pthread_create(&cid, NULL, consumer, NULL);
+
+    pthread_join(pid, NULL);
+    pthread_join(cid, NULL);
+    
+    return 0;
+}
+
+void *producer(void *p)
+{
+    struct msg *mp;
+    struct msg *tmp;
+
+    for (; ; ) 
+    {
+        mp = malloc(sizeof(struct msg));
+
+        mp->num = rand() % 1000 + 1;
+        printf("Produce %d\n", mp->num);
+        pthread_mutex_lock(&lock);
+        tmp = head;
+        while ((head != NULL) && (tmp->next != head)) 
+        {
+            tmp = tmp->next;
+        }
+        if (head != NULL) 
+        {
+            tmp->next = mp;
+        }
+        else
+        {
+            head = mp;
+        }
+        mp->next = head;
+        //head = mp;
+        pthread_mutex_unlock(&lock);
+        pthread_cond_signal(&has_product);
+        sleep(rand() % 5);
+    }
+}
+
+void *consumer(void *p)
+{
+    struct msg *mp;
+
+    for (; ; ) 
+    {
+        pthread_mutex_lock(&lock);
+        while (head == NULL) 
+        {
+            pthread_cond_wait(&has_product, &lock);
+        }
+        mp = head;
+        head = mp->next;
+        pthread_mutex_unlock(&lock);
+        printf("Consume %d\n", mp->num);
+        free(mp);
+        sleep(rand() % 5);
+    }
+}
---
+++ wb/pthread_eat.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 5
+#define L(x) (x)
+#define R(x) (((x) + 1) % N)
+
+pthread_mutex_t chopstick[N];
+
+void *philosophy(void *arg);
+void pickup(int me);
+void drop(int me);
+
+int main(int argc, const char *argv[])
+{
+    pthread_t tid[N];
+    int i, p[N];
+
+    srand(time(NULL));
+
+    for (i = 0; i < N; i++) 
+    {
+        pthread_mutex_init(&chopstick[i], NULL);
+    }
+
+    for (i = 0; i < N; i++) 
+    {
+        p[i] = i;
+    }
+
+    for (i = 0; i < N-1; i++) 
+    {
+        pthread_create(&tid[i], NULL, philosophy, (void *)p[i]);
+    }
+    philosophy((void *)(N-1));
+    
+    for (i = 0; i < N-1; i++) 
+    {
+        pthread_join(tid[i], NULL);
+    }
+    for (i = 0; i < N - 1; i++) 
+    {
+        pthread_mutex_destroy(&chopstick[i]);
+    }
+    return 0;
+}
+
+void pickup(int me)
+{
+    pthread_mutex_lock(&chopstick[L(me)]);
+    pthread_mutex_lock(&chopstick[R(me)]);
+}
+
+void drop(int me)
+{
+    pthread_mutex_unlock(&chopstick[L(me)]);
+    pthread_mutex_unlock(&chopstick[R(me)]);
+}
+
+void *philosophy(void *arg)
+{
+    int me = (int)arg;
+
+    while (1) 
+    {
+        printf("%d is thinking....\n", me);
+        usleep(rand() % 10);
+        pickup(me);
+        printf("%d is eating....\n", me);
+        usleep(rand() % 10);
+        drop(me);
+    }
+}
