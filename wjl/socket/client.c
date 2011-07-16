e9234dc8305acd47322f92df1ac7e41129946aae
XiaoGuo@xwp <guozesheng@gmail.com>
2011-07-16 14:41:38 +0800

    wrap.h

--- wjl/socket/client.c
+++ wjl/socket/client.c
@@ -7,6 +7,7 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <unistd.h>
+#include "wrap.h"
 
 #define MAXLINE 80
 #define SERV_PORT 8000
@@ -27,31 +28,31 @@ int main(int argc, const char *argv[])
     str = (char *)argv[1];
 #endif
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockfd = my_socket(AF_INET, SOCK_STREAM, 0);
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
     servaddr.sin_port = htons(SERV_PORT);
 
-    connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+    my_connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
 
     while (fgets(buf, MAXLINE, stdin) != NULL)
     {
-        write(sockfd, buf, strlen(buf) + 1);
+        my_write(sockfd, buf, strlen(buf) + 1);
 
-        n = read(sockfd, buf, MAXLINE);
+        n = my_read(sockfd, buf, MAXLINE);
         if (n == 0)
         {
             printf("The other side has been closed.\n");
         }
         else 
         {
-            write(STDOUT_FILENO, buf, n);
+            my_write(STDOUT_FILENO, buf, n);
         }
     }
     
-    close(sockfd);
+    my_close(sockfd);
     printf("\n");
     return 0;
 }
--- wjl/socket/server.c
+++ wjl/socket/server.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <arpa/inet.h>
+#include "wrap.h"
 
 #define MAXLINE 80
 #define SERV_PORT 8000
@@ -19,27 +20,27 @@ int main(int argc, const char *argv[])
     char str[INET_ADDRSTRLEN];
     int n, i;
 
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    listenfd = my_socket(AF_INET, SOCK_STREAM, 0);
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(SERV_PORT);
 
-    bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+    my_bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
 
-    listen(listenfd, 20);
+    my_listen(listenfd, 20);
 
     printf("Accepting connections...\n");
 
     while (1) 
     {
         cliaddr_len = sizeof(cliaddr);
-        connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &cliaddr_len);
+        connfd = my_accept(listenfd, (struct sockaddr *)&cliaddr, &cliaddr_len);
 
         while (1) 
         {
-            n = read(connfd, buf, MAXLINE);
+            n = my_read(connfd, buf, MAXLINE);
             if (n == 0)
             {
                 printf("The other side has been closed.\n");
@@ -51,10 +52,10 @@ int main(int argc, const char *argv[])
             {
                 buf[i] = toupper(buf[i]);
             }
-            write(connfd, buf, n);
+            my_write(connfd, buf, n);
         }
 
-        close(connfd);
+        my_close(connfd);
     }
 
     return 0;
