40d20893756058450bbd8654de5fdde0a0897cb4
XiaoGuo@xwp <guozesheng@gmail.com>
2011-07-17 14:24:28 +0800

    modified

--- wjl/sqlite/main.c
+++ wjl/sqlite/main.c
@@ -7,13 +7,23 @@ int main(int argc, const char *argv[])
     int rc;
     char sql[4096];
     char *errmsg = NULL;
+    char **result;
+    int nr = 0, nc = 0;
+    int i, j;
 
     rc = sqlite3_open("mydb", &db);
-    sprintf(sql, "SELECT * FROM mydb");
-    rc = sqlite3_exec(db, sql, 0, 0, &errmsg);
-    if (rc == SQLITE_OK) 
+    sprintf(sql, "SELECT * FROM mytable");
+    printf("%s\n", sql);
+
+    sqlite3_get_table(db, sql, &result, &nr, &nc, &errmsg);
+
+    for (i = 1; i < nr + 1; i++) 
     {
-        printf("Successful!\n");
+        for (j = 0; j < nc; j++) 
+        {
+            printf("%s, ", result[i*nc + j]);
+        }
+        printf("\n");
     }
     
     return 0;
