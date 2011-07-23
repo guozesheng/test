bf5c3ebae121f4c95945dfeeb99a405c5f6b4ad1
XiaoGuo@xwp <guozesheng@gmail.com>
2011-07-23 15:21:06 +0800

    vector.cpp

---
+++ wjl/qt/vector.cpp
@@ -0,0 +1,23 @@
+#include <QApplication>
+#include <QVector>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    int i;
+
+    QVector(QString> vector(0));
+
+    vector.append("one");
+    vector.append("two");
+    vector.append("three");
+
+    for (i = 0; i < vector.count(); i++) 
+    {
+        qDebug()<<vector[i]<<endl;
+    }
+
+    return a.exec();
+}
