3a6f1510c46fb3ac3ebbce36a290518755e41984
XiaoGuo@xwp <guozesheng@gmail.com>
2011-07-29 15:13:13 +0800

    sql

---
+++ wjl/socket/file/sql.sql
@@ -0,0 +1,7 @@
+CREATE TABLE pic (id integer primary key, picname varchar(90), picsub varchar(1000));
+
+INSERT INTO pic VALUES (0, "wang.jpg", "Hello, world!");
+INSERT INTO pic VALUES (1, "wang2.jpg", "Hello, world!");
+INSERT INTO pic VALUES (3, "smile.jpg", "Some smile's cost is hurt themselves.");
+
+SELECT * FROM pic;
