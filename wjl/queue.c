2ec8229775ea82537886b124b4777bc47a65adb9
XiaoGuo@xwp <guozesheng@gmail.com>
2011-08-02 11:44:13 +0800

    queue

---
+++ wjl/queue.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct stu
+{
+    int id;
+    int num;
+    struct stu *next;
+} NODE_stu;
+
+NODE_stu *queue_insert(NODE_stu *head);
+int queue_print(NODE_stu *head);
+NODE_stu *qinput(NODE_stu *p);
+
+int main(int argc, const char *argv[])
+{
+    NODE_stu *head = NULL;
+
+    head = queue_insert(head);
+    head = queue_insert(head);
+
+    queue_print(head);
+    
+    return 0;
+}
+
+NODE_stu *queue_insert(NODE_stu *head)
+{
+    NODE_stu *p; 
+    NODE_stu *ptr = NULL;
+
+    p = head;
+
+    if (p == NULL) 
+    {
+        p = qinput(ptr);
+        return p;
+    }
+
+    while (p->next != NULL) 
+    {
+        p = p->next;
+    }
+
+    p->next = qinput(ptr);
+    
+    return head;
+}
+
+NODE_stu *qinput(NODE_stu *p)
+{
+    NODE_stu *ptr;
+    
+    ptr = (NODE_stu *)malloc(sizeof(NODE_stu));
+    if (ptr == NULL) 
+    {
+        perror("malloc");
+        exit(1);
+    }
+    printf("Please input the id:");
+    scanf("%d", &ptr->id);
+    printf("Please input the num:");
+    scanf("%d", &ptr->num);
+    ptr->next = NULL;
+
+    p = ptr;
+    return p;
+}
+
+int queue_print(NODE_stu *head)
+{
+    NODE_stu *p = head;
+
+    while (p != NULL) 
+    {
+        printf("id:%d\t", p->id);
+        printf("num:%d\n", p->num);
+        p = p->next;
+    }
+    
+    return 0;
+}
